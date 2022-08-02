8194767fd1af4f1eacbe173a78acf5ff0ac2b99a
Jack-Ricketts <zygga.k@gmail.com>
2022-08-03 00:53:37 +0300

    gsap sandbox wip

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,17 @@
 import "../../style.css";
 
-import gsap, { Power3 } from "gsap";
+import gsap from "gsap";
+
+gsap.to(".hour", {
+  rotation: 360,
+  duration: 12,
+  ease: "none",
+  repeat: -1,
+});
+
+gsap.to(".min", {
+  rotation: 360,
+  duration: 0.5,
+  ease: "none",
+  repeat: -1,
+});
