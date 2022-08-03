854d0b6d709769dbaa0f094edf9a3f48fbeacabd
Jack-Ricketts <zygga.k@gmail.com>
2022-08-03 15:39:10 +0300

    clock wip

--- src/js/app.js
+++ src/js/app.js
@@ -2,41 +2,58 @@ import "../../style.css";
 
 import gsap from "gsap";
 
-gsap.to(".hour", {
-  rotation: 360,
-  duration: 12,
-  ease: "none",
-  repeat: -1,
-});
+//automated clock-----------------
 
-gsap.to(".min", {
-  rotation: 360,
-  duration: 0.5,
-  ease: "none",
-  repeat: -1,
-});
-
-// gsap.to("body", {
-//   repeatDelay: 12,
+// gsap.to(".hour", {
+//   rotation: 360,
 //   duration: 12,
+//   ease: "none",
 //   repeat: -1,
+// });
+
+// gsap.to(".min", {
+//   rotation: 360,
+//   duration: 0.5,
+//   ease: "none",
+//   repeat: -1,
+// });
+
+// let tl = gsap.timeline({ repeat: -1 });
+
+// tl.to("body", {
+//   duration: 12,
 //   backgroundColor: "#3883fc",
 // });
-// gsap.to("body", {
-//   delay: 12,
+
+// tl.to("body", {
 //   duration: 12,
-//   repeat: -1,
 //   backgroundColor: "#030117",
 // });
 
-let tl = gsap.timeline({ repeat: -1 });
+//mousewheel clock-----------------
 
-tl.to("body", {
-  duration: 12,
-  backgroundColor: "#3883fc",
-});
+let shiftHour = 0;
+let shiftMinute = 0;
+const body = document.querySelector("body");
+const el = document.querySelector(".clock");
+const h = document.querySelector(".hour");
+const m = document.querySelector(".min");
 
-tl.to("body", {
-  duration: 12,
-  backgroundColor: "#030117",
-});
+let b = 0;
+let c = 5;
+
+function timeShift(event) {
+  event.preventDefault();
+  shiftHour += event.deltaY * 0.04;
+  shiftMinute += event.deltaY * 0.96;
+  b += event.deltaY * 0.0225;
+  c += event.deltaY * 0.028;
+  b = Math.min(Math.max(0, b), 200);
+  c = Math.min(Math.max(0, c), 255);
+  h.style.transform = `rotate(${shiftHour}deg)`;
+  m.style.transform = `rotate(${shiftMinute}deg)`;
+  body.style.backgroundColor = "rgb(" + 0 + "," + b + "," + c + ")";
+  console.log(b, c);
+}
+
+el.onwheel = timeShift;
